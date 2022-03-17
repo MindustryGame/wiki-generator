12e3e4ecb35cea7634f47597ad495e6269a99648
MEEP of Faith <meepmatthew@gmail.com>
2022-03-17 11:08:52 -0700

    Block Attribute Display

--- src/wikigen/generators/BlockGenerator.java
+++ src/wikigen/generators/BlockGenerator.java
@@ -2,12 +2,14 @@ package wikigen.generators;
 
 import arc.files.*;
 import arc.struct.*;
+import mindustry.*;
 import mindustry.content.*;
 import mindustry.ctype.*;
 import mindustry.world.*;
 import mindustry.world.blocks.*;
 import mindustry.world.blocks.environment.*;
 import mindustry.world.blocks.legacy.*;
+import mindustry.world.meta.*;
 import wikigen.*;
 
 @Generates(ContentType.block)
@@ -20,8 +22,24 @@ public class BlockGenerator extends FileGenerator<Block>{
             "liquidDrop", block instanceof Floor f ? f.liquidDrop : null,
             "speedMultiplier", block instanceof Floor f && f.speedMultiplier != 1f ? (int)(f.speedMultiplier * 100) + "%" : null,
             "dragMultiplier", block instanceof Floor f && f.dragMultiplier != 1f ? (int)(f.dragMultiplier * 100) + "%" : null,
-            "status", block instanceof Floor f && f.status != StatusEffects.none ? f.status : null
+            "status", block instanceof Floor f && f.status != StatusEffects.none ? f.status : null,
+            "affinities", scrapeAttributes(block)
         );
+    }
+
+    public String scrapeAttributes(Block block){
+        StringBuilder s = new StringBuilder();
+        for(Attribute a : Attribute.all){
+            if(block.attributes.get(a) != 0){
+                if(block instanceof Floor){
+                    if(!s.isEmpty()) s.append("\n");
+                    s.append(a.name).append(": ").append((int)(block.attributes.get(a) * 100)).append("%");
+                }else{
+                    s.append(links(Vars.content.blocks().select(b -> b.minfo.mod == null && b.attributes.get(a) != 0)));
+                }
+            }
+        }
+        return s.toString();
     }
 
     @Override
